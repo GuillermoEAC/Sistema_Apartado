130eec065d93a962176181ce30407d8b4320b36e
GuillermoEAC <guillermochina12@gmail.com>
2026-05-19 22:55:06 -0700

    Backedn finalazida creo

--- backend/api/src/modules/calendario/calendario.module.ts
+++ backend/api/src/modules/calendario/calendario.module.ts
@@ -4,9 +4,13 @@ import { Reserva } from '../reservas/entities/reserva.entity';
 import { BloqueoHorario } from '../bloqueos/entities/bloqueo.entity';
 import { CalendarioService } from './calendario.service';
 import { CalendarioController } from './calendario.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Reserva, BloqueoHorario])],
+    imports: [
+        TypeOrmModule.forFeature([Reserva, BloqueoHorario]),
+        AuthModule,
+    ],
     controllers: [CalendarioController],
     providers: [CalendarioService],
 })
